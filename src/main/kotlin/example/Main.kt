eb5b5a9bd94d20c465a3930cddebfbd4992d7f1b
albertlatacz <albert.latacz@gmail.com>
2022-08-31 06:25:50 +0100

    playing around with schema registration

--- src/main/kotlin/example/Main.kt
+++ src/main/kotlin/example/Main.kt
@@ -1,9 +1,10 @@
 package example
 
+import com.github.avrokotlin.avro4k.Avro
+import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient
 import io.confluent.kafka.serializers.KafkaAvroSerializer
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerializationException
-import org.apache.avro.Schema
-import org.apache.avro.generic.GenericData
 import org.apache.avro.generic.GenericRecord
 import org.apache.kafka.clients.consumer.Consumer
 import org.apache.kafka.clients.consumer.ConsumerConfig
@@ -16,22 +17,41 @@ import java.util.Arrays
 import java.util.Properties
 
 
+@Serializable
+data class Item(val name: String)
+
+@Serializable
+data class Data(val name: String, val list: List<Item>)
+
+
 fun main() {
+    val topic = "topic4"
+    val schemaRegistryUrl = "http://localhost:8081"
+    val bootstrapServersUrl = "localhost:9092"
+
+    val registryClient = CachedSchemaRegistryClient(schemaRegistryUrl, 10000)
+
+    val dataSchema = Avro.default.schema(Data.serializer())
+    println("dataSchema = ${dataSchema.toString(true)}")
+
+    val register = registryClient.register(topic, dataSchema)
+    println("register = ${register}")
+
+    val dataRecord = Avro.default.toRecord(Data.serializer(), Data("Name1", listOf(Item("Name2"))))
+
     val props = Properties()
-    props[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9092"
+    props[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServersUrl
     props[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
     props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = KafkaAvroSerializer::class.java
-    props["schema.registry.url"] = "http://localhost:8081"
+    props["schema.registry.url"] = schemaRegistryUrl
+    props["auto.register.schemas"] = false
+
     val producer = KafkaProducer<String, GenericRecord>(props)
+
     val key = "key1"
-    val userSchema = "{\"type\":\"record\"," +
-        "\"name\":\"myrecord\"," +
-        "\"fields\":[{\"name\":\"f1\",\"type\":\"string\"}]}"
-    val parser = Schema.Parser()
-    val schema = parser.parse(userSchema)
-    val avroRecord: GenericRecord = GenericData.Record(schema)
-    avroRecord.put("f1", "value1")
-    val record = ProducerRecord("topic1", key, avroRecord)
+    val record = ProducerRecord(topic, key, dataRecord)
+
+
     try {
         producer.send(record)
     } catch (e: SerializationException) {
@@ -43,22 +63,19 @@ fun main() {
         producer.close()
     }
 
-
-
     // Consumer
     val props2 = Properties()
 
-    props2[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9092"
+    props2[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServersUrl
     props2[ConsumerConfig.GROUP_ID_CONFIG] = "group1"
 
 
     props2[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringDeserializer"
     props2[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = "io.confluent.kafka.serializers.KafkaAvroDeserializer"
-    props2["schema.registry.url"] = "http://localhost:8081"
+    props2["schema.registry.url"] = schemaRegistryUrl
 
     props2[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
 
-    val topic = "topic1"
     val consumer: Consumer<String, GenericRecord> = KafkaConsumer(props2)
     consumer.subscribe(Arrays.asList(topic))
 
